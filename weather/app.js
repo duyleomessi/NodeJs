daadff1559151159b3b89dbe021d01c9cb8993d2
duyleomessi <duyleitbka95@gmail.com>
2016-04-19 08:29:58 +0700

    main application

---
+++ weather/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by root on 12/04/2016.
+ */
+
+'use strict';
+
+var https = require('https');
+
+var lat;
+var lng;
+
+var coordinate = require('./models/coordinate');
+
+coordinate.get(lat, lng);
+
+
+
